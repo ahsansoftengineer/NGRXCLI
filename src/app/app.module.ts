0e186ced06533ac96eaeb2a9e96de68e9e1f8a89
Muhammad Ahsan <mahsansoftengineer@indusvalley.io>
2021-05-07 14:04:43 +0500

    CLI Commands Runs for NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { UserEffects } from './user.effects';
 // import {MaterialModule} from './material.module';
 
 
@@ -20,6 +26,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([UserEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
