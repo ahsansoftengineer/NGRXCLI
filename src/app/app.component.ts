28137054c49245ab3103901ae2e83032aeb3976f
Muhammad Ahsan <mahsansoftengineer@indusvalley.io>
2021-05-07 13:22:32 +0500

    init NgRx CLI

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  `,
+  styles: [`
+  `]
+})
+export class AppComponent {
+  constructor() {
+  }
+}
+
+// my component are dependent on api service
+// api service -> http service
+// http service -> http client
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+// import {MaterialModule} from './material.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
